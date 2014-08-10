f9bd7be24f80c81f4337403eb71b4b1b406bd40a
Jakub Pavlík jn <severus@post.cz>
2014-08-10 23:50:41 +0200

    lyv lengthcheck works (somehow)

--- lib/lyv/lengthchecker.rb
+++ lib/lyv/lengthchecker.rb
@@ -7,42 +7,59 @@ module Lyv
 
   class LengthChecker
 
+    # returns false if length of lyrics corresponds to the length of music,
+    # otherwise returns Array of two Integers [music_length, lyrics_length]
     def check(score)
+      m = music_length score
+      l = lyrics_length score
 
-    end
-
-    def check_music(music)
-
-    end
-
-    def check_file(path)
-
+      if m == l then
+        return false
+      else
+        return [m, l]
+      end
     end
 
     def music_length(score)
       m = score.music
-      m_content = m[m.index('{')+1 .. m.rindex('}')-1]
+
+      begin
+        m_content = m[m.index('{')+1 .. m.rindex('}')-1]
+      rescue NoMethodError
+        # either brace not found
+        return 0
+      end
 
       l = 0
       in_melisma = false
-      m_content.scan(/[cdefgab](is|es)?[',]*[12486]{0,2}([\(\)~])?/) do |accidental, mel|
-        if mel == '~' then
-          next
-        elsif mel == '(' then
-          in_melisma = true
-          next
-        elsif mel == ')' then
-          in_melisma = false
-        end
 
-        l += 1
+      # remove key signature
+      m_content = m_content.gsub(/\\key [cdefgab](is|es)? \\\w+/, '')
+
+      m_content.split(/\s+/).each do |token|
+        /^[cdefgab](is|es)?[',]*[12486]{0,2}\.?([\(\)~])?([_\-\^].*)?$/.match(token) do |match|
+          accidental, mel = match[1], match[2]
+
+          if mel == '~' then
+            next
+          elsif mel == '(' then
+            in_melisma = true
+            next
+          elsif mel == ')' then
+            in_melisma = false
+          elsif in_melisma then
+            next
+          end
+
+          l += 1
+        end
       end
 
       return l
     end
 
     def lyrics_length(score)
-      score.lyrics_raw.split(/\s+/).select {|x| x != '--' }.size
+      score.lyrics_raw.strip.split(/\s+/).select {|x| x != '--' }.size
     end
   end
 end
--- spec/lengthchecker_spec.rb
+++ spec/lengthchecker_spec.rb
@@ -15,8 +15,32 @@ module Lyv
       @notes_ly = "\\score { \\relative c' { a4 b c, d8. e'' f4 g4 a bes cis2 } \\addlyrics { a -- hoj a -- no ne la -- la -- la moo_m -- tsa } }"
       @notes_score = LilyPondScore.new @notes_ly
 
-      @melisma_ly = "\\score { \\relative c' { a4~ a b,( d) } }"
+      @melisma_ly = "\\score { \\relative c' { a4~ a b,( c e c d) } }"
       @melisma_score = LilyPondScore.new @melisma_ly
+
+      @real_ly = <<-EOS
+\\score {
+  \\relative c'' {
+    \\choralniRezim
+    g4 a g b c d c \\barMaior
+    c4( b a) b c( d) e d \\barMin
+    d4 f e d( c) a b a g g \\barFinalis
+  }
+  \\addlyrics {
+    Hos -- po -- di -- ne, slyš můj hlas,
+    ja -- ko ka -- di -- dlo ať k_to -- bě stou -- pá má mo -- dlit -- ba.
+  }
+  \\header {
+    quid = "1. ant."
+    modus = "VII"
+    differentia = "a"
+    psalmus = "Žalm 141"
+    id = "1ne-ant1"
+    piece = \\markup {\\sestavTitulek}
+  }
+}
+EOS
+      @real_score = LilyPondScore.new @real_ly
     end
 
     describe '#music_length' do
@@ -31,11 +55,36 @@ module Lyv
       it 'counts melisma as one note' do
         @c.music_length(@melisma_score).should eq 2
       end
+
+      it 'does not count variables' do
+        var_ly = "\\score { \\relative c' { \\choralniRezim g4 a \\barMaior } }"
+        var_score = LilyPondScore.new var_ly
+        @c.music_length(var_score).should eq 2
+      end
+
+      it 'copes with a real score' do
+        @c.music_length(@real_score).should eq 21
+      end
+
+      it 'does not count key signature' do
+        key_ly = "\\score { \\relative c' { \\key f \\major } }"
+        key_score = LilyPondScore.new key_ly
+        @c.music_length(key_score).should eq 0
+      end
+
+      it 'handles correctly note with markup' do
+        markup_score = LilyPondScore.new "\\score { \\relative c' { d^\\markup\\rubrVelikAleluja } }"
+        @c.music_length(markup_score).should eq 1
+      end
     end
 
     describe '#lyrics_length' do
       it 'works well' do
         @c.lyrics_length(@notes_score).should eq 10
+      end
+
+      it 'copes with a real score' do
+        @c.lyrics_length(@real_score).should eq 21
       end
     end
 
