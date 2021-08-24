67ceb7a3625fbbcf5e1f74e099f369aceffa1ca0
Jakub Pavlik <severus@post.cz>
2021-08-24 12:53:22 +0200

    specs: instance variable -> let

--- spec/lilypondscore_spec.rb
+++ spec/lilypondscore_spec.rb
@@ -6,8 +6,9 @@ module Lyv
   
   describe LilyPondScore do
 
-    before :each do
-      @score_with_comments_source = '\score {
+    let :score_with_comments do
+      LilyPondScore.new \
+'\score {
   \relative c\'\' {
     \choralniRezim
     c a c c a g f \barMin
@@ -25,19 +26,18 @@ module Lyv
     piece = \markup {\sestavTitulek}
   }
 }'
-      @score_with_comments = LilyPondScore.new @score_with_comments_source
     end
 
     describe "#text" do
       it 'preserves comments' do
-        @score_with_comments.text.should include '% Hle'
+        score_with_comments.text.should include '% Hle'
       end
     end
       
     describe '#lyrics_raw' do
       it 'has comments stripped' do
-        @score_with_comments.lyrics_raw.should_not include '%'
-        @score_with_comments.lyrics_raw.should eq 'Ej -- hle, Hos -- po -- din při -- jde'
+        score_with_comments.lyrics_raw.should_not include '%'
+        score_with_comments.lyrics_raw.should eq 'Ej -- hle, Hos -- po -- din při -- jde'
       end
 
       it 'handles nested braced expressions correctly' do
@@ -48,29 +48,29 @@ module Lyv
 
     describe '#lyrics_readable' do
       it 'has comments stripped' do
-        @score_with_comments.lyrics_readable.should_not include '%'
-        @score_with_comments.lyrics_readable.should eq 'Ejhle, Hospodin přijde'
+        score_with_comments.lyrics_readable.should_not include '%'
+        score_with_comments.lyrics_readable.should eq 'Ejhle, Hospodin přijde'
       end
     end
 
     describe '#header' do
       it 'does not contain comments' do
-        @score_with_comments.header['psalmus'].should eq 'Žalm 142'
+        score_with_comments.header['psalmus'].should eq 'Žalm 142'
       end
     end
 
     describe '#music' do
       it 'starts with the opening token of the music' do
-        @score_with_comments.music.should start_with "\\relative c''"
+        score_with_comments.music.should start_with "\\relative c''"
       end
 
       it 'ends with the closing brace' do
-        @score_with_comments.music.should end_with '}'
+        score_with_comments.music.should end_with '}'
       end
 
       it 'contains music' do
-        @score_with_comments.music.should include '\choralniRezim'
-        @score_with_comments.music.should include 'c a c c a g f \barMin'
+        score_with_comments.music.should include '\choralniRezim'
+        score_with_comments.music.should include 'c a c c a g f \barMin'
       end
     end
   end
